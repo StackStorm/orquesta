58f2dd3999f39989ed672679f980180151b0294d
W Chan <m4d.coder@gmail.com>
2016-04-11 18:07:07 -0700

    Implement basic graphing for workflow execution
    Implement WorkflowScore to graph the workflow execution paths.

---
+++ orchestra/symphony.py
@@ -0,0 +1,50 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+import networkx as nx
+
+
+LOG = logging.getLogger(__name__)
+
+
+class WorkflowScore(object):
+
+    def __init__(self):
+        self._graph = nx.DiGraph()
+
+    def show(self):
+        return nx.to_dict_of_dicts(self._graph)
+
+    def get_start_tasks(self):
+        return [n for n, d in self._graph.in_degree().items() if d == 0]
+
+    def has_task(self, task):
+        return self._graph.has_node(task)
+
+    def add_task(self, task):
+        if not self.has_task(task):
+            self._graph.add_node(task)
+
+    def has_sequence(self, source, destination):
+        return self._graph.has_edge(source, destination)
+
+    def add_sequence(self, source, destination):
+        if not self.has_task(source):
+            self.add_task(source)
+
+        if not self.has_task(destination):
+            self.add_task(destination)
+
+        if not self.has_sequence(source, destination):
+            self._graph.add_edge(source, destination)
---
+++ orchestra/tests/unit/test_workflow_score.py
@@ -0,0 +1,87 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from orchestra import symphony
+
+
+EXPECTED_GRAPH = {
+    'task1': {
+        'task2': {},
+        'task4': {},
+        'task7': {}
+    },
+    'task2': {
+        'task3': {}
+    },
+    'task3': {
+        'task5': {}
+    },
+    'task4': {
+        'task5': {}
+    },
+    'task5': {
+        'task6': {}
+    },
+    'task6': {},
+    'task7': {
+        'task8': {}
+    },
+    'task8': {}
+}
+
+
+class WorkflowScoreTest(unittest.TestCase):
+
+    def _add_tasks(self, score):
+        for i in range(1, 9):
+            score.add_task('task' + str(i))
+
+    def _add_sequences(self, score):
+        score.add_sequence('task1', 'task2')
+        score.add_sequence('task2', 'task3')
+        score.add_sequence('task1', 'task4')
+        score.add_sequence('task3', 'task5')
+        score.add_sequence('task4', 'task5')
+        score.add_sequence('task5', 'task6')
+        score.add_sequence('task1', 'task7')
+        score.add_sequence('task7', 'task8')
+
+    def test_basic_graph(self):
+        score = symphony.WorkflowScore()
+        self._add_tasks(score)
+        self._add_sequences(score)
+
+        self.assertDictEqual(EXPECTED_GRAPH, score.show())
+
+    def test_skip_add_tasks(self):
+        score = symphony.WorkflowScore()
+        self._add_sequences(score)
+
+        self.assertDictEqual(EXPECTED_GRAPH, score.show())
+
+    def test_duplicate_add_tasks(self):
+        score = symphony.WorkflowScore()
+        self._add_tasks(score)
+        self._add_tasks(score)
+        self._add_sequences(score)
+
+        self.assertDictEqual(EXPECTED_GRAPH, score.show())
+
+    def test_duplicate_add_sequences(self):
+        score = symphony.WorkflowScore()
+        self._add_tasks(score)
+        self._add_sequences(score)
+        self._add_sequences(score)
+
+        self.assertDictEqual(EXPECTED_GRAPH, score.show())
