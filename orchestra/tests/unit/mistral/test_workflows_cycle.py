8aa4425f1947d40590d48c6bab943610230d5df1
W Chan <m4d.coder@gmail.com>
2016-05-23 12:38:03 -0700

    Add test case for multiple cycles in a workflow

--- orchestra/tests/unit/mistral/test_workflows_cycle.py
+++ orchestra/tests/unit/mistral/test_workflows_cycle.py
@@ -181,3 +181,250 @@ class CyclicWorkflowConductorTest(base.WorkflowConductorTest):
             expected_task_seq,
             contexts=contexts
         )
+
+    def test_cycles(self):
+        wf_name = 'cycles'
+
+        expected_wf_graph = {
+            'directed': True,
+            'graph': {},
+            'nodes': [
+                {
+                    'id': 'prep'
+                },
+                {
+                    'id': 'task1'
+                },
+                {
+                    'id': 'task2'
+                },
+                {   
+                    'id': 'task3'
+                },
+                {
+                    'id': 'task4'
+                },
+                {
+                    'id': 'task5'
+                }
+            ],
+            'adjacency': [
+                [
+                    {
+                        'id': 'task1',
+                        'criteria': self._get_seq_expr(
+                            'prep',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task2',
+                        'criteria': self._get_seq_expr(
+                            'task1',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task3',
+                        'criteria': self._get_seq_expr(
+                            'task2',
+                            states.SUCCESS,
+                            'not $.proceed'
+                        )
+                    },
+                    {
+                        'id': 'task5',
+                        'criteria': self._get_seq_expr(
+                            'task2',
+                            states.SUCCESS,
+                            '$.proceed'
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task4',
+                        'criteria': self._get_seq_expr(
+                            'task3',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task2',
+                        'criteria': self._get_seq_expr(
+                            'task4',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task1',
+                        'criteria': self._get_seq_expr(
+                            'task5',
+                            states.SUCCESS,
+                            "$.count < 3"
+                        )
+                    }
+                ]
+            ],
+            'multigraph': False
+        }
+
+        self._assert_wf_graph(wf_name, expected_wf_graph)
+
+        expected_wf_ex_graph = {
+            'directed': True,
+            'graph': {},
+            'nodes': [
+                {
+                    'id': 'prep',
+                    'name': 'prep'
+                },
+                {
+                    'id': 'task1',
+                    'name': 'task1'
+                },
+                {
+                    'id': 'task2',
+                    'name': 'task2'
+                },
+                {
+                    'id': 'task3',
+                    'name': 'task3'
+                },
+                {
+                    'id': 'task4',
+                    'name': 'task4'
+                },
+                {
+                    'id': 'task5',
+                    'name': 'task5'
+                }
+            ],
+            'adjacency': [
+                [
+                    {
+                        'id': 'task1',
+                        'criteria': self._get_seq_expr(
+                            'prep',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task2',
+                        'criteria': self._get_seq_expr(
+                            'task1',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task3',
+                        'criteria': self._get_seq_expr(
+                            'task2',
+                            states.SUCCESS,
+                            'not $.proceed'
+                        )
+                    },
+                    {
+                        'id': 'task5',
+                        'criteria': self._get_seq_expr(
+                            'task2',
+                            states.SUCCESS,
+                            '$.proceed'
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task4',
+                        'criteria': self._get_seq_expr(
+                            'task3',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task2',
+                        'criteria': self._get_seq_expr(
+                            'task4',
+                            states.SUCCESS
+                        )
+                    }
+                ],
+                [
+                    {
+                        'id': 'task1',
+                        'criteria': self._get_seq_expr(
+                            'task5',
+                            states.SUCCESS,
+                            "$.count < 3"
+                        )
+                    }
+                ]
+            ],
+            'multigraph': False
+        } 
+
+        self._assert_compose(wf_name, expected_wf_ex_graph)
+
+        expected_task_seq = [
+            'prep',
+            'task1',
+            'task2',
+            'task3',
+            'task4',
+            'task2',
+            'task5',
+            'task1',
+            'task2',
+            'task3',
+            'task4',
+            'task2',
+            'task5',
+            'task1',
+            'task2',
+            'task3',
+            'task4',
+            'task2',
+            'task5'
+        ]
+
+        contexts = [
+            {'count': 0},                   # prep
+            {'count': 0, 'proceed': False}, # task1
+            {'count': 0, 'proceed': False}, # task2
+            {'count': 0, 'proceed': False}, # task3
+            {'count': 0, 'proceed': True},  # task4
+            {'count': 0, 'proceed': True},  # task2
+            {'count': 1, 'proceed': True},  # task5
+            {'count': 1, 'proceed': False}, # task1
+            {'count': 1, 'proceed': False}, # task2
+            {'count': 1, 'proceed': False}, # task3
+            {'count': 1, 'proceed': True},  # task4
+            {'count': 1, 'proceed': True},  # task2
+            {'count': 2, 'proceed': True},  # task5
+            {'count': 2, 'proceed': False}, # task1
+            {'count': 2, 'proceed': False}, # task2
+            {'count': 2, 'proceed': False}, # task3
+            {'count': 2, 'proceed': True},  # task4
+            {'count': 2, 'proceed': True},  # task2
+            {'count': 3, 'proceed': True}   # task5
+        ]
+
+        self._assert_conduct(
+            expected_wf_ex_graph,
+            expected_task_seq,
+            contexts=contexts
+        )
