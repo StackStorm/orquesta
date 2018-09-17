327f61a4656f7735bf9d51b6c11395eebf1e9aa2
W Chan <m4d.coder@gmail.com>
2018-09-17 12:39:15 -0700

    Add facade to get list of regex patterns for finding expressions
    Add a function in the expression facade for returning a list of regex patterns for finding expressions.

--- orquesta/expressions/yql.py
+++ orquesta/expressions/yql.py
@@ -75,6 +75,10 @@ class YAQLEvaluator(base.Evaluator):
         return ctx
 
     @classmethod
+    def get_statement_regex(cls):
+        return cls._regex_pattern
+
+    @classmethod
     def has_expressions(cls, text):
         exprs = cls._regex_parser.findall(text)
 
