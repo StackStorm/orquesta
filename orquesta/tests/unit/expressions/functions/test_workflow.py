3ef536033295675a96797a433d4f6226c0620d63
W Chan <m4d.coder@gmail.com>
2019-02-22 12:15:48 -0800

    Fix unit tests for workflow functions used in expressions
    Fix the unit tests associated with the workflow functions such as succeeded(), failed(), task_state(), and etc. that are used in YAQL and Jinja expressions. The internal data structure of the conductor has changed and these functions need to be refactored to read the data structure correctly.

--- orquesta/tests/unit/expressions/functions/test_workflow.py
+++ orquesta/tests/unit/expressions/functions/test_workflow.py
@@ -12,6 +12,7 @@
 
 import unittest
 
+from orquesta import constants
 from orquesta import exceptions as exc
 from orquesta.expressions.functions import workflow as funcs
 from orquesta import states
@@ -20,40 +21,60 @@ from orquesta import states
 class WorkflowFunctionTest(unittest.TestCase):
 
     def test_task_state_empty_context(self):
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+        task_flow_entry = {'id': task_name, 'route': task_route}
+
         context = None
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
         context = {}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
         context = {'__flow': None}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
         context = {'__flow': {}}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
         context = {'__flow': {'tasks': None}}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
         context = {'__flow': {'tasks': {}}}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
-        context = {'__flow': {'tasks': {'t1': None}}}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        task_pointers = {task_flow_pointer_id: None}
+        context = {'__flow': {'tasks': task_pointers}}
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
-        context = {'__flow': {'tasks': {'t1': 0}, 'sequence': [None]}}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.UNSET)
+        task_pointers = {task_flow_pointer_id: 0}
+        context = {'__flow': {'tasks': task_pointers, 'sequence': [None]}}
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.UNSET)
 
     def test_task_state_dereference_errors(self):
-        context = {'__flow': {'tasks': {'t1': 0}}}
-        self.assertRaises(IndexError, funcs.task_state_, context, 't1')
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+        task_flow_entry = {'id': task_name, 'route': task_route}
 
-        context = {'__flow': {'tasks': {'t1': 1}, 'sequence': [{'state': states.RUNNING}]}}
-        self.assertRaises(IndexError, funcs.task_state_, context, 't1')
+        task_pointers = {task_flow_pointer_id: 0}
+        context = {'__flow': {'tasks': task_pointers}}
+        self.assertRaises(IndexError, funcs.task_state_, context, task_flow_entry)
+
+        task_pointers = {task_flow_pointer_id: 1}
+        context = {'__flow': {'tasks': task_pointers, 'sequence': [{'state': states.RUNNING}]}}
+        self.assertRaises(IndexError, funcs.task_state_, context, task_flow_entry)
 
     def test_task_state(self):
-        context = {'__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.RUNNING}]}}
-        self.assertEqual(funcs.task_state_(context, 't1'), states.RUNNING)
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+        task_flow_entry = {'id': task_name, 'route': task_route}
+
+        task_pointers = {task_flow_pointer_id: 0}
+        context = {'__flow': {'tasks': task_pointers, 'sequence': [{'state': states.RUNNING}]}}
+        self.assertEqual(funcs.task_state_(context, task_flow_entry), states.RUNNING)
 
     def test_get_current_task_empty(self):
         self.assertRaises(exc.ExpressionEvaluationException, funcs._get_current_task, None)
@@ -65,77 +86,117 @@ class WorkflowFunctionTest(unittest.TestCase):
         self.assertRaises(exc.ExpressionEvaluationException, funcs._get_current_task, context)
 
     def test_get_current_task(self):
-        context = {'__current_task': {'name': 't1'}}
+        context = {'__current_task': {'id': 't1', 'route': 0}}
         self.assertDictEqual(funcs._get_current_task(context), context['__current_task'])
 
     def test_succeeded(self):
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+
         context = {'__current_task': None}
         self.assertRaises(exc.ExpressionEvaluationException, funcs.succeeded_, context)
 
         context = {
-            '__current_task': {'id': 't1'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.RUNNING}]}
+            '__current_task': {'id': task_name, 'route': task_route},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.RUNNING}]
+            }
         }
 
         self.assertFalse(funcs.succeeded_(context))
 
         context = {
-            '__current_task': {'id': 't1'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.SUCCEEDED}]}
+            '__current_task': {'id': task_name, 'route': task_route},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.SUCCEEDED}]
+            }
         }
 
         self.assertTrue(funcs.succeeded_(context))
 
     def test_failed(self):
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+
         context = {'__current_task': None}
         self.assertRaises(exc.ExpressionEvaluationException, funcs.failed_, context)
 
         context = {
-            '__current_task': {'id': 't1'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.RUNNING}]}
+            '__current_task': {'id': task_name, 'route': task_route},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.RUNNING}]
+            }
         }
 
         self.assertFalse(funcs.failed_(context))
 
         context = {
-            '__current_task': {'id': 't1'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.FAILED}]}
+            '__current_task': {'id': task_name, 'route': task_route},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.FAILED}]
+            }
         }
 
         self.assertTrue(funcs.failed_(context))
 
     def test_completed(self):
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+
         context = {'__current_task': None}
         self.assertRaises(exc.ExpressionEvaluationException, funcs.completed_, context)
 
         context = {
-            '__current_task': {'id': 't1'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.RUNNING}]}
+            '__current_task': {'id': task_name, 'route': task_route},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.RUNNING}]
+            }
         }
 
         self.assertFalse(funcs.completed_(context))
 
         context = {
-            '__current_task': {'id': 't1'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.SUCCEEDED}]}
+            '__current_task': {'id': task_name, 'route': task_route},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.SUCCEEDED}]
+            }
         }
 
         self.assertTrue(funcs.completed_(context))
 
     def test_result(self):
+        task_route = 0
+        task_name = 't1'
+        task_flow_pointer_id = constants.TASK_FLOW_ROUTE_FORMAT % (task_name, str(task_route))
+
         context = {'__current_task': None}
         self.assertRaises(exc.ExpressionEvaluationException, funcs.result_, context)
 
         context = {
-            '__current_task': {'id': 't1', 'result': 'foobar'},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.SUCCEEDED}]}
+            '__current_task': {'id': task_name, 'route': task_route, 'result': 'foobar'},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.SUCCEEDED}]
+            }
         }
 
         self.assertEqual(funcs.result_(context), 'foobar')
 
         context = {
-            '__current_task': {'id': 't1', 'result': {'fu': 'bar'}},
-            '__flow': {'tasks': {'t1': 0}, 'sequence': [{'state': states.SUCCEEDED}]}
+            '__current_task': {'id': task_name, 'route': task_route, 'result': {'fu': 'bar'}},
+            '__flow': {
+                'tasks': {task_flow_pointer_id: 0},
+                'sequence': [{'state': states.SUCCEEDED}]
+            }
         }
 
         self.assertDictEqual(funcs.result_(context), {'fu': 'bar'})
