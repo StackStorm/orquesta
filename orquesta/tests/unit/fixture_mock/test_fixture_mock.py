b89c02ceca3e265d3fd63d83adf7bc9441128043
guzzijones12 <guzzijones12@gmail.com>
2020-06-11 20:42:38 +0000

    fix python2 import mock

--- orquesta/tests/unit/fixture_mock/test_fixture_mock.py
+++ orquesta/tests/unit/fixture_mock/test_fixture_mock.py
@@ -1,5 +1,7 @@
-
-from unittest import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 from orquesta.specs.mock.models import TestFileSpec
 from orquesta.fixture_mock  import Fixture
 from orquesta.tests.unit.conducting.native import base
