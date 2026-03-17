0e5e442f4ade1829451bd70ce04b66e78fe476d0
Carlos <nzlosh@yahoo.com>
2026-03-17 08:09:25 +0100

    Bump pip/setuptools to support python version from 3.10 to 3.12

--- setup.py
+++ setup.py
@@ -62,10 +62,9 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     entry_points={
         "orquesta.composers": [
